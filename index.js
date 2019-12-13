a93f1153623b3525dd84bd37fcc8efa010a9baf6
Hilmar Gústafsson <hilmargustafs@gmail.com>
2019-12-13 16:44:40 +0100

    Initial version

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('child_process').exec;
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const project = core.getInput('project');
+  const passwd = core.getInput('password');
+  const profile = core.getInput('publish-profile');
+  const config = core.getInput('configuration');
+
+  var child = exec(`MSBuild.exe ${project} 
+    -p:DeployOnBuild=True
+    -p:Password=${password}
+    -p:PublishProfile="${profile}"
+    -p:Configuration=${config}`,
+    function (error, stdout, stderr) {
+      console.log(`stdout: ${stdout}\nstderr: ${stderr}\n`);
+      if (error != null) {
+        core.setFailed(error.message);
+      }
+    });
+  exec();
 } catch (error) {
   core.setFailed(error.message);
 }
