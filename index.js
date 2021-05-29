43e2a5505447995c91e0b79433296eae38a7f4ac
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2021-05-29 23:33:00 +0200

    Use `spawn` instead of `exec`

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const exec = require('child_process').exec;
+const { spawn } = require('child_process');
 
 try {
   const project = core.getInput('project');
-  const passwd = core.getInput('password');
+  const password = core.getInput('password');
   const profile = core.getInput('publish-profile');
   const config = core.getInput('configuration');
-
-  const cmd = `MSBuild.exe ${project} -p:Configuration=${config} -p:DeployOnBuild=True -p:PublishProfile="${profile}" -p:Password=${passwd}`;
   
-  var child = exec(cmd,
-    function (error, stdout, stderr) {
-      console.log(`stdout: ${stdout}\nstderr: ${stderr}\n`);
-      if (error != null) {
-        core.setFailed(error.message);
-      }
-    });
+  const msbuild = spawn("MSBuild.exe", [
+    '-p:DeployOnBuild=True',
+    `-p:Configuration=${config}`,
+    `-p:PublishProfile="${profile}"`,
+    `-p:Password=${password}`
+  ]);
+  
+  msbuild.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
+
 } catch (error) {
   core.setFailed(error.message);
 }
