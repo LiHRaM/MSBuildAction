64bd80e8cb596789a48bf7ff9c9a0f13d2dc3a61
Hilmar Gústafsson <LiHRaM@users.noreply.github.com>
2021-06-30 17:40:45 +0000

    Remove @actions/github

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
 const { spawn } = require('child_process');
 
 try {
@@ -7,16 +6,15 @@ try {
   const password = core.getInput('password');
   const profile = core.getInput('publish-profile');
   const config = core.getInput('configuration');
-  
+
   const msbuild = spawn('msbuild.exe', [
-      project,
-      '-p:DeployOnBuild=True',
-      `-p:Configuration=${config}`,
-      `-p:PublishProfile=${profile}`,
-      `-p:Password=${password}`,
-    ],
-    { stdio: 'inherit' });
-  
+    project,
+    '-p:DeployOnBuild=True',
+    `-p:Configuration=${config}`,
+    `-p:PublishProfile=${profile}`,
+    `-p:Password=${password}`,
+  ], { stdio: 'inherit' });
+
   msbuild.on('close', (code) => {
     process.exit(code);
   });
